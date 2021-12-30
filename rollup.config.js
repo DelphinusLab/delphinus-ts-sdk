06abfb113d0f737e072829428efa9b3f9b60cec7
Xin Gao <xgao@zoyoe.com>
2021-12-30 18:25:43 +0800

    add config-overrides.js

---
+++ rollup.config.js
@@ -0,0 +1,40 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import wasm from "@rollup/plugin-wasm";
+import json from "@rollup/plugin-json";
+import svg from 'rollup-plugin-svg';
+import dts from "rollup-plugin-dts";
+
+const packageJson = require("./package.json");
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      wasm(),
+      json(),
+      svg(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
+];
