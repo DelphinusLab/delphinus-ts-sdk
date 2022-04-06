c667732974bdfeb7f99f5035aa7bf5cc3ecf8450
Richard Hao <richard@rhao.io>
2022-04-06 12:10:40 +1000

    update login styles

--- src/lib/account.tsx
+++ src/lib/account.tsx
@@ -1,16 +1,16 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React from 'react';
+import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import Modal from "@mui/material/Modal";
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
 import { SxProps, Theme } from "@mui/material/styles";
-import { styled } from '@mui/material/styles';
+import { styled } from "@mui/material/styles";
 
 import MetaMaskLogo from "../icons/metamask.svg";
 import PolkaLogo from "../icons/polka.svg";
@@ -41,10 +41,10 @@ const style: SxProps<Theme> = {
 };
 
 export const TxDialog = styled(Dialog)(({ theme }) => ({
-  '& .MuiDialogContent-root': {
+  "& .MuiDialogContent-root": {
     padding: theme.spacing(2),
   },
-  '& .MuiDialogActions-root': {
+  "& .MuiDialogActions-root": {
     padding: theme.spacing(1),
   },
 }));
@@ -58,19 +58,17 @@ export const TxDialogTitle = (props: DialogTitleProps) => {
   const { children, ...other } = props;
 
   return (
-    <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+    <DialogTitle sx={{ mx: 5, my: 3 }} {...other}>
       {children}
     </DialogTitle>
   );
 };
 
-
-
 // href="https://metamask.io/download"
 
 interface IProps {
   name: string;
-  children: any;
+  children?: any;
 }
 
 export function SetAccount(props: IProps) {
@@ -88,42 +86,42 @@ export function SetAccount(props: IProps) {
       open={status !== "Ready"}
       aria-labelledby="customized-dialog-title"
     >
-      <TxDialogTitle id="customized-dialog-title" >
-        {props.name}
+      <TxDialogTitle id="customized-dialog-title">
+        <div className="home-title">
+          <img src={props.name} className="home-logo"></img>
+        </div>
       </TxDialogTitle>
       <DialogContent>
         {props.children}
         <DialogActions>
-          {l1Account === undefined && (
-            <Button
-              startIcon={<img src={MetaMaskLogo} className="chain-icon"></img>}
-              variant="contained"
-              onClick={() => dispatch(loginL1AccountAsync())}
-            >
-              Connect Wallet
-            </Button>
-          )}
-          {l1Account && (
-            <Button
-              startIcon={<img src={MetaMaskLogo} className="chain-icon"></img>}
-              variant="contained"
-              disabled
-            >
-              <Address address={l1Account!.address}></Address>
-            </Button>
-          )}
-          {
-            <Button
-              disabled={l1Account === undefined}
-              startIcon={<img src={PolkaLogo} className="chain-icon"></img>}
-              variant="contained"
-              onClick={() =>
-                l1Account && dispatch(loginL2AccountAsync(l1Account.address))
-              }
-            >
-              Sign In
-            </Button>
-          }
+          <div className="home-btn-wrapper">
+            {l1Account === undefined && (
+              <Button
+                className="home-btn"
+                variant="contained"
+                onClick={() => dispatch(loginL1AccountAsync())}
+              >
+                Connect Wallet
+              </Button>
+            )}
+            {l1Account && (
+              <Button className="home-btn" variant="contained" disabled>
+                <Address address={l1Account!.address}></Address>
+              </Button>
+            )}
+            {
+              <Button
+                disabled={l1Account === undefined}
+                className="home-btn"
+                variant="contained"
+                onClick={() =>
+                  l1Account && dispatch(loginL2AccountAsync(l1Account.address))
+                }
+              >
+                Sign In
+              </Button>
+            }
+          </div>
         </DialogActions>
       </DialogContent>
     </TxDialog>
