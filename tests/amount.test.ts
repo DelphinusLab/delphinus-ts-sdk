ba0154a26231e3006192ac4676ea370cc09749e8
yymone <yymone84@hotmail.com>
2022-05-17 23:49:16 +1000

    add unit test framework and unit tests for the amount.ts, confirm 18 wei is not working currently

---
+++ tests/amount.test.ts
@@ -0,0 +1,91 @@
+import * as Amount from "../src/lib/amount"
+import BN from "bn.js";
+import { TokenInfo } from "../src/lib/type";
+
+describe("test toAmountInput", () => {
+    test("General number case", () => {
+        let res: Amount.Amount = Amount.toAmountInput(1, 10);
+        expect(res).toEqual({
+            wei: 10,
+            amount: 1,
+            input: true
+        });
+    } 
+    );
+
+    test("e^18 number case", () => {
+        let res: Amount.Amount = Amount.toAmountInput(1, 18);
+        expect(res).toEqual({
+            wei: 18,
+            amount: 1,
+            input: true
+        });
+    } 
+    );
+}
+);
+
+describe("test fromPreciseWeiRepr", () => {
+    test("General number case", () => {
+        let res: Amount.Amount = Amount.fromPreciseWeiRepr(new BN("22000000000"), 10);
+        expect(res).toEqual({
+            wei: 10,
+            amount: 2.2,
+            input: false
+        });
+    } 
+    );
+
+    test("wei 18 case", () => {
+        let res: Amount.Amount = Amount.fromPreciseWeiRepr(new BN("1100000000000000000"), 18);
+        expect(res).toEqual({
+            wei: 18,
+            amount: 1.1,
+            input: false
+        });
+    } 
+    );
+}
+);
+
+describe("test toPreciseWeiRepr", () => {
+    test("General number case", () => {
+        let input: Amount.Amount = {
+            wei: 10,
+            amount: 2.2,
+            input: true
+        };
+        let res: BN = Amount.toPreciseWeiRepr(input);
+        expect(res).toEqual(new BN("22000000000"));
+    } 
+    );
+
+    test("Throw error case for input false", () => {
+       
+        let input: Amount.Amount = {
+            wei: 10,
+            amount: 2.2,
+            input: false
+        };
+        function toPreciseWeiReprThrow() {
+            Amount.toPreciseWeiRepr(input);
+        }
+        expect(toPreciseWeiReprThrow).toThrow();
+    } 
+    );
+
+
+
+    test("wei 18 case", () => {
+        let input: Amount.Amount = {
+            wei: 18,
+            amount: 1.1,
+            input: true
+        };
+        let res: BN = Amount.toPreciseWeiRepr(input);
+        expect(res).toEqual(new BN("1100000000000000000"));
+    } 
+    );
+}
+);
+
