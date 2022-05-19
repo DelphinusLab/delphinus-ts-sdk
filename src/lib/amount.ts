c940bab959a561657d42c24c9ba2044b24ef7584
Richard Hao <richard@rhao.io>
2022-05-19 19:08:27 +1000

    update wei conversion

--- src/lib/amount.ts
+++ src/lib/amount.ts
@@ -2,7 +2,8 @@ import BN from "bn.js";
 import { TokenInfo } from "./type";
 export interface Amount {
   wei: number;
-  amount: number;
+  amount: string;
+  raw: BN;
   input: boolean;
 }
 
@@ -28,71 +29,36 @@ export function fromPreciseWeiRepr(repr: BN, wei: number) {
     fraction = fraction.substring(0, fraction.length - 1);
   }
   //change to number type
-  const amount = Number(whole + "." + fraction);
+  const amount = whole + "." + fraction;
+  const raw = repr;
 
   return {
     wei: wei,
     amount: amount,
+    raw: raw,
     input: false,
   };
 }
 
-export function toPreciseWeiRepr(a: Amount) {
+export function toPreciseWeiRepr(a: Amount): BN {
   if (a.input == false) {
     throw new Error(
       "can not convert amount back into precise repr when the amount is not an input"
     );
-  } else if (a.amount < 0) {
-    throw new Error("amount less than zero");
   } else {
-    try {
-      //Ethersjs conversion function for reference.
-      //https://github.com/ethers-io/ethers.js/blob/8b62aeff9cce44cbd16ff41f8fc01ebb101f8265/packages/bignumber/src.ts/fixednumber.ts#L70
-      let multiplier = getMultiplier(a.wei); //Multiplier of converting number into wei units
-
-      const comps = a.amount.toString().split("."); //get whole and fractional parts of input amount
-      let whole = comps[0],
-        fraction = comps[1];
-
-      if (!whole) {
-        whole = "0";
-      }
-      if (!fraction) {
-        fraction = "0";
-      }
-      // Trim trailing zeros
-      while (fraction[fraction.length - 1] === "0") {
-        fraction = fraction.substring(0, fraction.length - 1);
-      }
-
-      if (fraction.length > multiplier.length - 1) {
-        throw new Error("fractional component exceeds decimals");
-      }
-
-      if (fraction === "") {
-        fraction = "0";
-      }
-
-      // Fully pad the string with zeros to get to wei
-      while (fraction.length < multiplier.length - 1) {
-        fraction += "0";
-      }
-
-      const wholeWei = new BN(whole)
-        .mul(new BN(multiplier))
-        .add(new BN(fraction));
-      return wholeWei;
-    } catch (err) {
-      //console.log(err);
-      throw new Error("Invalid BN - Please enter a valid number");
-    }
+    return a.raw;
   }
 }
 
-export function toAmountInput(a: number, wei: number): Amount {
+export function toAmountInput(a: string, wei: number): Amount {
+  //Validate Input string
+  //Set BN representation of Input
+  let raw = fractionalToBN(a, wei);
+
   return {
     wei: wei,
     amount: a,
+    raw: raw,
     input: true,
   };
 }
@@ -107,4 +73,47 @@ function getMultiplier(decimals: number): string {
     return "1" + zeros.substring(0, decimals);
   }
   throw new Error("Invalid decimal value");
+}
+
+function fractionalToBN(num: string, wei: number): BN {
+  let multiplier = getMultiplier(wei);
+
+  const negative = num.substring(0, 1) === "-";
+  if (negative) {
+    throw new Error("Negative number encountered");
+  }
+  const comps = num.split("."); //get whole and fractional parts of input amount
+  if (comps.length > 2) throw new Error("too many decimal points");
+
+  let whole = comps[0],
+    fraction = comps[1];
+
+  if (!whole) {
+    whole = "0";
+  }
+  if (!fraction) {
+    fraction = "0";
+  }
+
+  // Trim trailing zeros
+  while (fraction[fraction.length - 1] === "0") {
+    fraction = fraction.substring(0, fraction.length - 1);
+  }
+
+  if (fraction.length > multiplier.length - 1) {
+    throw new Error("fractional component exceeds decimals");
+  }
+
+  if (fraction === "") {
+    fraction = "0";
+  }
+
+  // Fully pad the string with zeros to get to wei
+  while (fraction.length < multiplier.length - 1) {
+    fraction += "0";
+  }
+
+  const full = new BN(whole).mul(new BN(multiplier)).add(new BN(fraction));
+
+  return full;
 }
--- tests/amount.test.ts
+++ tests/amount.test.ts
@@ -2,26 +2,6 @@ import * as Amount from "../src/lib/amount";
 import BN from "bn.js";
 import { TokenInfo } from "../src/lib/type";
 
-describe("test toAmountInput", () => {
-  test("General number case", () => {
-    let res: Amount.Amount = Amount.toAmountInput(1, 10);
-    expect(res).toEqual({
-      wei: 10,
-      amount: 1,
-      input: true,
-    });
-  });
-
-  test("e^18 number case", () => {
-    let res: Amount.Amount = Amount.toAmountInput(1, 18);
-    expect(res).toEqual({
-      wei: 18,
-      amount: 1,
-      input: true,
-    });
-  });
-});
-
 describe("test fromPreciseWeiRepr", () => {
   test("General number case", () => {
     let res: Amount.Amount = Amount.fromPreciseWeiRepr(
@@ -30,7 +10,8 @@ describe("test fromPreciseWeiRepr", () => {
     );
     expect(res).toEqual({
       wei: 10,
-      amount: 2.2,
+      amount: "2.2",
+      raw: new BN("22000000000"),
       input: false,
     });
   });
@@ -42,7 +23,8 @@ describe("test fromPreciseWeiRepr", () => {
     );
     expect(res).toEqual({
       wei: 18,
-      amount: 1.1,
+      amount: "1.1",
+      raw: new BN("1100000000000000000"),
       input: false,
     });
   });
@@ -54,29 +36,42 @@ describe("test fromPreciseWeiRepr", () => {
     );
     expect(res).toEqual({
       wei: 18,
-      amount: 1.0011,
+      amount: "1.0011",
+      raw: new BN("1001100000000000000"),
       input: false,
     });
   });
 
   test("wei 18 case 3", () => {
+    let res: Amount.Amount = Amount.fromPreciseWeiRepr(new BN("1100000"), 18);
+    expect(res).toEqual({
+      wei: 18,
+      amount: "0.0000000000011",
+      raw: new BN("1100000"),
+      input: false,
+    });
+  });
+
+  test("wei 18 case 4", () => {
     let res: Amount.Amount = Amount.fromPreciseWeiRepr(
-        new BN("1100000"),
-        18
-      );
-      expect(res).toEqual({
-        wei: 18,
-        amount: 0.0000000000011,
-        input: false,
-      });
+      new BN("10000000000000000000000000100000000000000001"),
+      18
+    );
+    expect(res).toEqual({
+      wei: 18,
+      amount: "10000000000000000000000000.100000000000000001",
+      raw: new BN("10000000000000000000000000100000000000000001"),
+      input: false,
+    });
   });
 });
 
-describe("test toPreciseWeiRepr", () => {
+describe("test toAmountInput", () => {
   test("General number case", () => {
     let input: Amount.Amount = {
       wei: 10,
-      amount: 2.2,
+      amount: "2.2",
+      raw: new BN(22000000000),
       input: true,
     };
     let res: BN = Amount.toPreciseWeiRepr(input);
@@ -86,7 +81,8 @@ describe("test toPreciseWeiRepr", () => {
   test("Throw error case for input false", () => {
     let input: Amount.Amount = {
       wei: 10,
-      amount: 2.2,
+      amount: "2.2",
+      raw: new BN("22000000000"),
       input: false,
     };
     function toPreciseWeiReprThrow() {
@@ -96,22 +92,45 @@ describe("test toPreciseWeiRepr", () => {
   });
 
   test("wei 18 case", () => {
-    let input: Amount.Amount = {
+    let input = {
       wei: 18,
-      amount: 1.1,
-      input: true,
+      amount: "1.1",
     };
-    let res: BN = Amount.toPreciseWeiRepr(input);
+    let res: BN = Amount.toAmountInput(input.amount, input.wei).raw;
+
     expect(res).toEqual(new BN("1100000000000000000"));
   });
 
   test("wei 18 case 2", () => {
-    let input: Amount.Amount = {
+    let input = {
       wei: 18,
-      amount: 0.0000000000011,
-      input: true,
+      amount: "1.000000000000011101",
     };
-    let res: BN = Amount.toPreciseWeiRepr(input);
-    expect(res).toEqual(new BN("1100000"));
+    let res: BN = Amount.toAmountInput(input.amount, input.wei).raw;
+
+    expect(res).toEqual(new BN("1000000000000011101"));
+  });
+
+  test("wei 18 large number (25 Zeros in whole number component)", () => {
+    let input = {
+      wei: 18,
+      amount: "10000000000000000000000000.000000000011101",
+    };
+    let res: BN = Amount.toAmountInput(input.amount, input.wei).raw;
+    console.log(res.toString());
+
+    expect(res).toEqual(new BN("10000000000000000000000000000000000011101000"));
+  });
+
+  test("throw too many decimals", () => {
+    let input = {
+      wei: 18,
+      amount: "1.0000000000000000000000000011101",
+    };
+    function toPreciseWeiReprThrow() {
+      Amount.toAmountInput(input.amount, input.wei);
+    }
+
+    expect(toPreciseWeiReprThrow).toThrow();
   });
 });
