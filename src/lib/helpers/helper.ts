429a09666f6b23b91353dd44b6770f0d7450b009
Richard Hao <richard@rhao.io>
2022-09-19 18:23:36 +1000

    cap number to 8 sig digits

--- src/lib/helpers/helper.ts
+++ src/lib/helpers/helper.ts
@@ -23,3 +23,29 @@ export function getPercentageBN(bn: BN, total: BN, precision: number) {
     minimumFractionDigits: 2,
   });
 }
+
+//function to cap a number from string format to a certain significant digits
+export function capNumber(num: string, significantDigits: number = 8) {
+  if (num === undefined || num === null) return num;
+
+  let parts = num.toString().split(".");
+  console.log(num, parts, "parts");
+  if (parts[0].length > significantDigits) {
+    return parts[0].substring(0, significantDigits);
+  }
+  let decimalsToShow = significantDigits - parts[0].length;
+
+  if (parts[1] && parts[1].length > decimalsToShow) {
+    //check number of leading 0s in parts[1]
+    let zeros = 0;
+    for (let i = 0; i < parts[1].length; i++) {
+      if (parts[1][i] === "0") zeros++;
+      else break;
+    }
+    //show only the first significant digit past the zeros if too many zeros
+    if (parts[0] === "0") return "0" + "." + parts[1].substring(0, zeros + 1);
+    //otherwise return capped number
+    return parts[0] + "." + parts[1].substring(0, decimalsToShow);
+  }
+  return num;
+}
